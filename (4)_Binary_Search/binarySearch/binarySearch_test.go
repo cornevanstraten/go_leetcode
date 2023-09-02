83f905fea8ffe43fe3510d81d2dbba88d157f933
Corné van Straten <corne.straten@array.com>
2023-09-02 14:53:24 -0400

    add binarySearch and tests

---
+++ (4)_Binary_Search/binarySearch/binarySearch_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+func TestTableBinarySearch(t *testing.T) {
+	var tests = []struct {
+		input1   []int
+		input2   int
+		expected int
+	}{
+		{[]int{-1, 0, 3, 5, 9, 12}, 9, 4},
+		{[]int{-1, 0, 3, 5, 9, 12}, 2, -1},
+	}
+
+	for i, test := range tests {
+		if output := BinarySearch(test.input1, test.input2); output != test.expected {
+			t.Errorf("Test %d failed", i)
+		}
+	}
+}
